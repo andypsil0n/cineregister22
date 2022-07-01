77f85b92f09aec8ac2b25f633639970b4d29237f
Andypsil0n <andypsil0n@gmx.de>
2022-07-01 09:36:06 +0200

    Added Tests to "MovieTest" and built the string-output for movie-times.

--- src/main/java/hwr/oop/Movie.java
+++ src/main/java/hwr/oop/Movie.java
@@ -1,4 +1,124 @@
 package hwr.oop;
 
+import java.util.*;
+
 public class Movie {
+    private final String name;
+    private final ArrayList<String> times;
+    private ArrayList<ArrayList<String>> sortedTimes = new ArrayList<ArrayList<String>>();
+
+    public Movie(String name, ArrayList times) {
+        this.name = name;
+        this.times = times;
+        sortTimes();
+    }
+
+    private void sortTimes() {
+        String show;
+        String[] s;
+
+        for (int x = 0; x < times.size(); x++) {
+            show = times.get(x);
+            s = show.split("/");
+
+            sortedTimes.add(x, new ArrayList<String>());
+            for (int y = 0; y < 4; y++) {
+                sortedTimes.get(x).add(s[y]);
+            }
+
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    private int getMaxYSize() {
+        int maxYSize = 1;
+        String currentMaxHolder = "";
+        for (int x = 0; x < sortedTimes.size(); x++) {
+            if (x != 0 && sortedTimes.get(x-1).get(0).equals(sortedTimes.get(x).get(0)) &!
+                    sortedTimes.get(x).get(0).equals(currentMaxHolder)) {
+                maxYSize += 1;
+                currentMaxHolder = sortedTimes.get(x).get(0);
+            } else if (currentMaxHolder.equals(sortedTimes.get(x).get(0))) {
+                maxYSize += 1;
+            }
+        }
+
+        return maxYSize;
+    }
+
+    private String getClockTimesAsString(int maxYSize, ArrayList<ArrayList<String>> tableRest) {
+        String stringClockTimes = "";
+
+        for (int i = 1; i <= maxYSize; i++) {
+            String line = "";
+
+
+            for (int x = 0; x < times.size(); x++) {
+                if (x != 0 && tableRest.get(x - 1).get(0).equals(tableRest.get(x).get(0))) {
+                    if (tableRest.get(x - 1).get(1).equals("")) {
+                        if (x == tableRest.size() - 1) {
+                            line = line + tableRest.get(x).get(1) + "\n";
+                            tableRest.get(x).set(1, "     \n");
+                        } else {
+                            line = line + tableRest.get(x).get(1) + "  ";
+                            tableRest.get(x).set(1, "     ");
+                        }
+                    }
+                    tableRest.get(x - 1).set(1, "");
+                } else {
+                    if (x == tableRest.size() - 1) {
+                        line = line + tableRest.get(x).get(1) + "\n";
+                        tableRest.get(x).set(1, "     \n");
+                    } else if (tableRest.get(x).get(1).equals("")) {
+                        line = line;
+                    } else {
+                        line = line + tableRest.get(x).get(1) + "  ";
+                        tableRest.get(x).set(1, "     ");
+                    }
+                }
+            }
+            stringClockTimes = stringClockTimes + line;
+
+        }
+        return stringClockTimes;
+    }
+
+
+    public String getTimes() {
+        String stringDays = "";
+        for (int x = 0; x < times.size(); x++) {
+            if (x == 0) {
+                stringDays = stringDays + sortedTimes.get(x).get(0) + "     ";
+            } else if (sortedTimes.get(x-1).get(0).equals(sortedTimes.get(x).get(0))) {
+                stringDays = stringDays;
+            } else if (x == times.size()-1) {
+                stringDays = stringDays + sortedTimes.get(x).get(0) + "\n";
+            } else {
+                stringDays = stringDays + sortedTimes.get(x).get(0) + "     ";
+            }
+        }
+
+        int maxYSize = getMaxYSize();
+
+        ArrayList<ArrayList<String>> tableRest = new ArrayList<ArrayList<String>>();
+        for (int x = 0; x < times.size(); x++) {
+            tableRest.add(new ArrayList<String>());
+            for (int y = 0; y < 2; y++) {
+                tableRest.get(x).add(this.sortedTimes.get(x).get(y));
+            }
+        }
+
+        String stringTimes = stringDays + getClockTimesAsString(maxYSize, tableRest);
+
+        return stringTimes;
+    }
+
+
+    public Hall getHallAt(String day, String time) {
+        return null;
+    }
 }
--- src/test/java/hwr/oop/MovieTest.java
+++ src/test/java/hwr/oop/MovieTest.java
@@ -1,4 +1,58 @@
 package hwr.oop;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
 public class MovieTest {
+    String name;
+    ArrayList<String> times;
+    String movieTimesAsString;
+
+    @BeforeEach
+    void setUp() {
+        name = "Star Wars VI";
+        times = new ArrayList<>(Arrays.asList ( "Mo/17:00/Saal 1/7.00","Mo/20:00/Saal 2/7.00","Di/17:00/Saal 1/8.00","Mi/14:00/Saal 2/7.50",
+                "Do/14:00/Saal 1/7.00","Fr/17:00/Saal 3/7.00","Sa/14:00/Saal 1/7.00",
+                "So/15:00/Saal 1/7.00"));
+
+        movieTimesAsString ="Mo     Di     Mi     Do     Fr     Sa     So\n" +
+                            "17:00  17:00  14:00  14:00  17:00  14:00  15:00\n" +
+                            "20:00                                          \n\n";
+    }
+
+    @Test
+    void movie_getName_returnsName() {
+        Movie movie = new Movie(name, times);
+        String movieName = movie.getName();
+        assertThat(movieName).isEqualTo(name);
+
+    }
+
+    @Test
+    void movie_getTimes_returnsTimesAsString() {
+        Movie movie = new Movie(name, times);
+        String movieTimes = movie.getTimes();
+        assertThat(movieTimes).isEqualTo(movieTimesAsString);
+    }
+
+   /* @Test
+    void movie_getHallAt_returnsWantedHallWithCorrectPrice() {
+        Movie movie = new Movie(name, times);
+        Hall movieHall = movie.getHallAt("Mo", "17:00");
+
+        Double wantedPrice = 7.00;
+        Double price = movieHall.getPrice();
+
+        assertThat(price).isEqualTo(wantedPrice);
+    }*/
+
+    @Test
+    void movie_getHallAt_returnsWantedHall() {
+
+    }
+
 }
