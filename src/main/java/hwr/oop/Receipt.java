cd75445954218aabb25afc681ad9f07886597d5d
Andypsil0n <andypsil0n@gmx.de>
2022-07-05 11:09:57 +0200

    Changed and added tests and functionality to "Receipt". Deleted "Example" and changed Project-Name in "pom.xml"

--- src/main/java/hwr/oop/Receipt.java
+++ src/main/java/hwr/oop/Receipt.java
@@ -1,13 +1,15 @@
 package hwr.oop;
 
+import java.util.ArrayList;
+
 public class Receipt {
-private String movieName;
-private int row;
-private int number;
-private double price;
-private int state;
-private String clientName;
-    public void addMovieName(String movieName) {
+    private String movieName;
+    private ArrayList<Seat> seats = new ArrayList<>();
+    private String clientName;
+    private String showDates;
+    private String hall;
+
+    public void setMovieName(String movieName) {
         this.movieName = movieName;
     }
 
@@ -15,16 +17,36 @@ private String clientName;
         return movieName;
     }
 
-    public void addSeatInformation(int row, int number, double price, int state) {
-        this.row = row;
-        this.number = number;
-        this.price = price;
-        this.state = state;
+    public void setShowDates(String showDates) {
+        this.showDates = showDates;
     }
 
-    public Seat getSeatInformation() {
-        return new Seat(row,number,price,state);
+    public String getShowDates() {
+        return showDates;
     }
+
+    public void setHall(String hall) {
+        this.hall = hall;
+    }
+
+    public String getHall() {
+        return hall;
+    }
+
+
+    public void addSeat(Seat seat) {
+        seats.add(seat);
+    }
+
+    public double getTotalCosts() {
+        double totalCosts = 0;
+
+        for (int seatCount = 0; seatCount < seats.size(); seatCount++) {
+            totalCosts = totalCosts + seats.get(seatCount).getPrice();
+        }
+        return totalCosts;
+    }
+
 
     public void addClientName(String clientName) {
         this.clientName= clientName;
@@ -34,7 +56,61 @@ private String clientName;
         return clientName;
     }
 
-    public String getReceiptAsString(String clientName) {
-        return("Client Name: "+ clientName);
+    public String getReceiptAsString() {
+        String titleLine = "Rechnung\n";
+        String freeLine = "\n";
+        
+        String movieLine = "Film:    " + movieName + "\n";
+        String showLine =  "Zeit:    " + showDates + "\n";
+        String hallLine =  "Saal:    " + hall + "\n";
+        
+        String seperatorLine = "-------------------------------\n";
+        
+        String seatsAsString = "";
+        seatsAsString = getSeatsAsStrin();
+        
+        String completePriceLine = "Gesamt:                  " + getPriceAString(getTotalCosts()) + "\n";
+        
+        String statusLine = "Status:  ";
+        Seat seat = seats.get(0);
+        if (seat.getStateAsChar() == 'r') {
+            statusLine = statusLine + "reserviert und unbezahlt\n";
+        } else if (seat.getStateAsChar() == '#') {
+            statusLine = statusLine + "gebucht und bezahlt\n";
+        } else {
+            statusLine = statusLine + "konnte nicht ermittelt werden\n";
+        }
+            
+        String nameLine = "Käufer:  " + clientName + "\n";
+        
+        String receiptAsString = "";
+        
+        receiptAsString = titleLine + freeLine + movieLine + showLine + hallLine + seperatorLine + seatsAsString +
+                freeLine + completePriceLine + statusLine + seperatorLine + nameLine;
+        
+        return receiptAsString;
     }
+
+    private String getSeatsAsStrin() {
+        String seatsAsString = "";
+
+        for (int seatCount = 0; seatCount < seats.size(); seatCount++) {
+            Seat seat = seats.get(seatCount);
+
+            String priceString = getPriceAString(seat.getPrice());
+
+            String seatString = "Sitz " + (seatCount+1) + ":  " + "Reihe " + seat.getRow() +
+                    " Platz " + seat.getNumber() + ", " + priceString + "\n";
+            seatsAsString = seatsAsString + seatString;
+        }
+
+        return seatsAsString;
+    }
+
+    private String getPriceAString(double price) {
+        String priceString = String.format("%.2f€", price);
+        return priceString;
+    }
+
+
 }
