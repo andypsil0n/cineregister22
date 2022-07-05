3c787f4fe1abffb9bdfe5f897638373beb62cd0b
Andypsil0n <andypsil0n@gmx.de>
2022-07-05 11:09:57 +0200

    Changed and added tests and functionality to "Receipt". Deleted "Example" and changed Project-Name in "pom.xml"

--- src/test/java/hwr/oop/MainTest.java
+++ src/test/java/hwr/oop/MainTest.java
@@ -9,19 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MainTest {
     @BeforeEach
     void setUp () {
-        String movie = "Dr. Strange";
-    }
-    @Test
-    void getSelectedMovie() {
 
     }
-    @Test
-    void getSelectedShow() {
 
-    }
-    @Test
-    void getReservedSeats() {
-
-    }
 
 }
--- src/test/java/hwr/oop/ReceiptTest.java
+++ src/test/java/hwr/oop/ReceiptTest.java
@@ -3,53 +3,141 @@ package hwr.oop;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReceiptTest {
+    private String wantedReceiptAsString;
+    private Seat seat1 = new Seat(5, 7, 7.00, '#');
+    private Seat seat2 = new Seat(5, 8, 7.00, '#');
+    private ArrayList<Seat> seats = new ArrayList();
+
+    @BeforeEach
+    void setUp() {
+        wantedReceiptAsString = "Rechnung\n" +
+                                "\n" +
+                                "Film:    Doctor Strange\n" +
+                                "Zeit:    Mo/17:00\n" +
+                                "Saal:    hall1\n" +
+                                "-------------------------------\n" +
+                                "Sitz 1:  Reihe 5 Platz 7, 7,00€\n" +
+                                "Sitz 2:  Reihe 5 Platz 8, 7,00€\n" +
+                                "\n" +
+                                "Gesamt:                  14,00€\n" +
+                                "Status:  gebucht und bezahlt\n" +
+                                "-------------------------------\n" +
+                                "Käufer:  Bernd Baum\n";
+
+        seats.add(seat1);
+        seats.add(seat2);
+    }
+
     @Test
     void receipt_get_totalSeatCosts_returns_totalPrice() {
-        Seat seat1 = new Seat(1, 1, 7.00, 2);
-        Seat seat2 = new Seat(1, 1, 7.00, 2);
-        assertThat(seat1.getPrice()+seat2.getPrice()).isEqualTo(14.00);
+        Receipt receipt = new Receipt();
+        for (int seatCount = 0; seatCount < seats.size(); seatCount++) {
+            receipt.addSeat(seats.get(seatCount));
+        }
+        double totalCosts = receipt.getTotalCosts();
+
+        assertThat(totalCosts).isEqualTo(14.00);
     }
+
+    /*
     @Test // Eingaben die beim bezahlen gemacht werden auch überprüfen?
     void receipt_getCreditCardNumber_length_returns16digits() {
         String creditCard_Number ="4356353291238672";
         assertThat(creditCard_Number.length()).isEqualTo(16);
-    }
+    } */
+
     @Test
-    void receipt_addMovieName_addsMovieNameToReceipt() {
+    void receipt_setMovieName_getMovieNameReturnsCorrectMovieName() {
         Receipt receipt = new Receipt();
-        receipt.addMovieName("Dr. Strange");
+        receipt.setMovieName("Dr. Strange");
         String name = receipt.getMovieName();
         assertThat(name).isEqualTo("Dr. Strange");
     }
+
     @Test
-    void receipt_addShowDates_addsShowDatesToReceipt() {
+    void receipt_setShowDates_getShowDatesReturnsCorrectShowDates() {
         Receipt receipt = new Receipt();
+        receipt.setShowDates("Mo/17:00");
+        assertThat(receipt.getShowDates()).isEqualTo("Mo/17:00");
     }
+
+    @Test
+    void receipt_setHall_getHallReturnsCorrectHall() {
+        Receipt receipt = new Receipt();
+        receipt.setHall("hall1");
+        assertThat(receipt.getHall()).isEqualTo("hall1");
+    }
+
+    /*
     @Test
     void receipt_addSeatInformation_addsSeatInformationToReceipt() {
         Receipt receipt = new Receipt();
-        receipt.addSeatInformation(1,1,7.00,3);
+        receipt.addSeatInformation(1,1,7.00,'#');
         Seat seat = receipt.getSeatInformation();
-        assertThat(seat).isEqualTo(new Seat(1,1,7.00,3));
+        assertThat(seat).isEqualTo(new Seat(1,1,7.00,'#'));
         // vllt als String und nicht als seat?
-    }
+    } */
+
     @Test
-    void receipt_addClientName_addsClientNameToReceipt() {
+    void receipt_addClientName_getClientNameReturnsCorrectClientName() {
         Receipt receipt = new Receipt();
         receipt.addClientName("Pascal Stark");
         String clientName = receipt.getClientName();
         assertThat(clientName).isEqualTo("Pascal Stark");
     }
+
     @Test
-    void receipt_getReceiptAsString_returnsReceiptAsString() {
+    void receipt_getReceiptAsString_returnsReceiptAsStringWithBookedSeats() {
         Receipt receipt = new Receipt();
-        String receiptAsStringManuel = "Client Name: Pascal Stark";
-        receipt.addClientName("Pascal Stark");
-        String receiptAsString = receipt.getReceiptAsString(receipt.getClientName());
-        assertThat(receiptAsStringManuel).isEqualTo(receiptAsString);
-        // wie stellt man das am besten dar? momentan nur mit client name
+
+        receipt.setMovieName("Doctor Strange");
+        receipt.setShowDates("Mo/17:00");
+        receipt.setHall("hall1");
+
+        receipt.addSeat(seat1);
+        receipt.addSeat(seat2);
+
+        receipt.addClientName("Bernd Baum");
+
+        String receiptAsString = receipt.getReceiptAsString();
+        assertThat(receiptAsString).isEqualTo(wantedReceiptAsString);
+
+    }
+
+    @Test
+    void receipt_getReceiptAsString_returnsReceiptAsStringWithReservedSeats() {
+        String wantedReceiptAsString = "Rechnung\n" +
+                "\n" +
+                "Film:    Doctor Strange\n" +
+                "Zeit:    Mo/17:00\n" +
+                "Saal:    hall1\n" +
+                "-------------------------------\n" +
+                "Sitz 1:  Reihe 5 Platz 7, 7,00€\n" +
+                "Sitz 2:  Reihe 5 Platz 8, 7,00€\n" +
+                "\n" +
+                "Gesamt:                  14,00€\n" +
+                "Status:  reserviert und unbezahlt\n" +
+                "-------------------------------\n" +
+                "Käufer:  Bernd Baum\n";;
+
+        Receipt receipt = new Receipt();
+
+        receipt.setMovieName("Doctor Strange");
+        receipt.setShowDates("Mo/17:00");
+        receipt.setHall("hall1");
+
+        receipt.addSeat(new Seat(5, 7, 7.00, 'r'));
+        receipt.addSeat(new Seat(5, 8, 7.00, 'r'));
+
+        receipt.addClientName("Bernd Baum");
+
+        String receiptAsString = receipt.getReceiptAsString();
+        assertThat(receiptAsString).isEqualTo(wantedReceiptAsString); // ändern
+
     }
 }
