0c78e79e43c12b50e8adea2cf6df05c37072db3a
Pascal Stark <pascalstark2003@outlook.de>
2022-07-05 09:04:06 +0200

    updated Main(test) + Receipt(test)

--- src/test/java/hwr/oop/MainTest.java
+++ src/test/java/hwr/oop/MainTest.java
@@ -1,4 +1,27 @@
 package hwr.oop;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MainTest {
+    @BeforeEach
+    void setUp () {
+        String movie = "Dr. Strange";
+    }
+    @Test
+    void getSelectedMovie() {
+
+    }
+    @Test
+    void getSelectedShow() {
+
+    }
+    @Test
+    void getReservedSeats() {
+
+    }
+
 }
